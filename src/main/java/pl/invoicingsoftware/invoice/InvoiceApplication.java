718945f39dda0adb9618e95ed074ca4ef50d2b3a
Patrycja StypuÅła <patistypula90@onet.eu>
2021-01-11 17:00:55 +0100

    first commit

---
+++ src/main/java/pl/invoicingsoftware/invoice/InvoiceApplication.java
@@ -0,0 +1,13 @@
+package pl.invoicingsoftware.invoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceApplication.class, args);
+	}
+
+}
