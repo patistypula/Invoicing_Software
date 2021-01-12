ff2a820ac9ef30a61aa7e33b1db6c564bc507823
Lukasz Wujciow <mieszko330.gmail.com>
2021-01-12 17:33:10 +0100

    Second commit

---
+++ src/main/java/pl/invoicingsoftware/invoice/home/HomeController.java
@@ -0,0 +1,13 @@
+package pl.invoicingsoftware.invoice.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String homePage(){
+        return "index";
+    }
+}
